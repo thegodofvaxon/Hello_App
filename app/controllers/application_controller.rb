6074e1862be705601d77d7071e7168e497775266
Daniel Pfeffer <daniel.pfeffer.09@gmail.com>
2021-06-14 22:48:40 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,57 @@
+class ApplicationController < ActionController::Base
+  def hello
+    render html: "
+1:Which bear is the most condescending?pan-duh!
+2:What kind of noise does a witch’s vehicle make? Brrrroooom, brrroooom.
+3:What’s brown and sticky? A stick.
+4:Two guys walked into a bar. The third guy ducked.
+5:How do you get a country girl’s attention? A tractor.
+6:Why are elevator jokes so classic and good? They work on many levels.
+7:What do you call a pudgy psychic? A four-chin teller.
+8:What did the police officer say to his belly-button? You’re under a vest.
+9:What do you call it when a group of apes starts a company? Monkey business.
+10:My wife asked me to stop singing “Wonderwall” to her. I said maybe…
+11:What kind of drink can be bitter and sweet? Reali-tea.
+12:What do you call a naughty lamb dressed up like a skeleton for Halloween? Baaad to the bone.
+13:Want to know why nurses like red crayons? Sometimes they have to draw blood.
+14:What would the Terminator be called in his retirement? The Exterminator.
+15:What did Tennessee? The same thing as Arkansas.
+16:My wife asked me to go get 6 cans of Sprite from the grocery store. I realized when I got home that I had picked 7 up.
+17:Why do bees have sticky hair? Because they use a honeycomb.
+18:Why do some couples go to the gym? Because they want their relationship to work out.
+19:What do you call an angry musician flipping someone off? A song bird.
+20:What’s the most detail-oriented ocean? The Pacific.
+21:Why is Peter Pan always flying? Because he Neverlands.
+22:Which state has the most streets? Rhode Island.
+23:What do you call 26 letters that went for a swim? Alphawetical.
+24:What’s the name of a very polite, European body of water? Merci.
+25:Why was the color green notoriously single? It was always so jaded.
+26:I used to hate facial hair, but then it grew on me.
+27:I want to make a brief joke, but it’s a little cheesy.
+28:Why did the coach go to the bank? To get his quarterback.
+29:How do celebrities stay cool? They have many fans.
+30:Sundays are always a little sad, but the day before is a sadder day.
+31:5/4 of people admit they’re bad at fractions.
+32:Why did the bedding hide their relationship? They just wanted something pillow-key!
+33:You’re American when you go into a bathroom and when you come out, but what are you while you’re in the bathroom? European.
+34:I’ve been thinking about taking up meditation. I figure it’s better than sitting around doing nothing.
+35:Dogs can’t operate MRI machines. But catscan.
+36:What did the flowers do when the bride walked down the aisle? They rose.
+37:It takes guts to be an organ donor.
+38:What does “Rockin’ Robin” do when she’s bored? Tweet.
+39:I lost my job at the bank on my first day. A woman asked me to check her balance, so I pushed her over.
+40:How do you row a canoe filled with puppies? Bring out the doggy paddle.
+41:Singing in the shower is fun until you get soap in your mouth. Then it becomes a soap opera.
+42:Why were the utensils stuck together? They were spooning.
+43:What’s a crafty dancer’s favorite hobby? Cutting a rug.
+44:How does a penguin build his house? Igloos it together.
+45:What kind of music do chiropractors like? Hip pop.
+46What kind of shoes does a lazy person wear? Loafers.
+47:Why is cold water so insecure? Because it’s never called hot.
+48:I was going to tell a time-traveling joke, but you guys didn’t like it.
+49:Shouldn’t the “roof” of your mouth actually be called the ceiling?
+50:Stop looking for the perfect match…use a lighter.
+51:I told my doctor I heard buzzing, but he said it’s just a bug going around.
+"
+  end
+end
